cf795e99963090495eeb3c9b0b3730a65a4b3ac7
ka52k <76809740+ka52k@users.noreply.github.com>
2021-03-20 14:22:35 +0900

    20210320-2変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,35 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      // console.log(res);
-    setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        // console.log(res);
+        // res.data.map((todo) => todo.title)
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         // <p>{todo.title}</p>
-        <Todo title={todo.title} userid={todo.userid} />
+        <Todo 
+          title={todo.title} 
+          userId={todo.userId} 
+          completed={todo.completed} 
+        />
       ))}
     </div>
   );
